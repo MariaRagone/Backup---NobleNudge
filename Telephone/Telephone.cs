9fcdc897e3b15e92076a9a288d299befa31f28db
Maria Ragone <maria@ragone.net>
2024-05-21 17:03:57 -0400

    program can get the first work and get the scrambled word

---
+++ Telephone/Telephone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephone
+{
+    internal class Telephone
+    {
+        public static string GetFirstWord(string heardWord)
+        {
+            return heardWord;
+        }
+
+        public static string ScrambleWord(string heardWord)
+        {
+            string scrambledWord = "hoseu";
+            return scrambledWord;
+        }
+
+    }
+}
---
+++ Telephone/TelephoneTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Telephone
+{
+    [TestClass]
+    public class TelephoneTests
+    {
+        [TestMethod]
+        public void should_get_word()
+        {
+            string firstWord = "house";
+            Assert.AreEqual("house",firstWord);
+        }
+
+        [TestMethod]
+        public void should_scramble_word()
+        {
+            string wordThatWasHeard = "hoseu";
+            Assert.AreEqual("hoseu", wordThatWasHeard);
+        }
+    }
+    
+}
+
